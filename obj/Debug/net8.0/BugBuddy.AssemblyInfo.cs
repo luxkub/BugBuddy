81b0f1343328deb76e76b608d0bb1627f50777b9
winnie <102517696+luxkub@users.noreply.github.com>
2024-02-02 11:34:43 -0500

    v1.3.0
    -Entering a resolution when editing automatically changes the status to "Closed" when saving.
    -Newest bugs are shown first.
    -There's a checkbox to display open bugs only.
    -Low priority bugs are displayed in green, medium priority bugs are displayed in orange, and high priority bugs are displayed in pink.

--- obj/Debug/net8.0/BugBuddy.AssemblyInfo.cs
+++ obj/Debug/net8.0/BugBuddy.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BugBuddy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+726a530aee97f220a548b2b82fba4fd0036e2ddf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f3f27b29442ae73531674cff57424dbcac0ae9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BugBuddy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BugBuddy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
